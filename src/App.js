fe501da053a62ae9a976e3474ed0b322f405ffdf
rabab-1993 <Rabab.Abdulaziz.elq.js@tuwaiq.edu.sa>
2021-11-03 21:07:31 +0300

    files added

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Nav from "./components/Nav";
+import User from "./components/User";
+import Result from "./components/Result";
+// import Description from "./components/Description";
+import Description from "./components/Description/Description";
+import "./style.css";
+
+const App = () => {
+  return (
+    <>
+      <Nav />
+      <Switch>
+        <Route exact path="/User" component={User} />
+        <Route exact path="/Result" component={Result} />
+        <Route exact path="/Description" component={Description} />
+        {/* <Route exact path="/Description" component={Description} /> */}
+      </Switch>
+
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Result/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./style.css";
+const Result = () => {
+  return (
+    <div className="grand">
+      <div className="resultBox">
+        <div className="up">
+          <h2>Congratulations</h2>
+          <h3>User Name</h3>
+          <h2>255</h2>
+        </div>
+
+        <div className="down">
+          <button id="restartBtn">Restart</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Result;
